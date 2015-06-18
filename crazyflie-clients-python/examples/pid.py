cb9591d67aca2a6f1e38948b7c07b1d42f070c91
Yi-Hsiu Liao <nee0224256667@yahoo.com.tw>
2015-06-18 14:09:11 +0800

    add param setting

--- crazyflie-clients-python/examples/pid.py
+++ crazyflie-clients-python/examples/pid.py
@@ -60,18 +60,37 @@ class PidExample:
     Simple logging example class that logs the Stabilizer from a supplied
     link uri and disconnects after 5s.
     """
-    DATA_NUM = 1000
+    DATA_NUM = 400
 
     log_var = [
             ("stabilizer.roll",   "float",   "radius", -10,  10),
             ("stabilizer.pitch",  "float",   "radius", -10,  10),
-            ("stabilizer.yaw",    "float",   "radius", -180, 180),
+           # ("stabilizer.yaw",    "float",   "radius", -180, 180),
             ("stabilizer.thrust", "uint16_t", "force",  0, 65535),
 
-            ("actuator.roll",     "int16_t",  "force",  0, 65535),
-            ("actuator.pitch",    "int16_t",  "force",  0, 65535)]
+            ("actuator.roll",     "int16_t",  "force",  -32768, 32767),
+            ("actuator.pitch",    "int16_t",  "force",  -32768, 32767)]
     log_data = []
     log_plot = []
+
+    param_var = [
+            ("pid_attitude", "roll_kp"),
+            ("pid_attitude", "roll_ki"),
+            ("pid_attitude", "roll_kd"),
+            ("pid_attitude", "pitch_kp"),
+            ("pid_attitude", "pitch_ki"),
+            ("pid_attitude", "pitch_kd"),
+            ("pid_attitude", "yaw_kp"),
+            ("pid_attitude", "yaw_ki"),
+            ("pid_attitude", "yaw_kd")
+            ]
+    param_spins = []
+
+
+    control_var = [ "thrust", "roll", "pitch", "yawrate" ]
+    control_spins = []
+
+    control_data = {}
 
     def __init__(self, link_uri):
         """ Initialize and run the example with the specified link_uri """
@@ -90,10 +109,15 @@ class PidExample:
         # Try to connect to the Crazyflie
         self._cf.open_link(link_uri)
 
-        self.thrust = 0
-        self.pitch = 0
-        self.roll = 0
-        self.yawrate = 0
+        #self.thrust = 0
+        #self.pitch = 0
+        #self.roll = 0
+        #self.yawrate = 0
+
+        self.control_data['roll'] = 0
+        self.control_data['pitch'] = 0
+        self.control_data['yawrate'] = 0
+        self.control_data['thrust'] = 0
 
         # setup gui
         self._gui()
@@ -102,6 +126,8 @@ class PidExample:
         for i in range(len(self.log_var)):
             self.log_data.append(deque([0] * self.DATA_NUM))
 
+        self._param_check_list = []
+        self._param_groups = []
 
     def _connected(self, link_uri):
         """ This callback is called form the Crazyflie API when a Crazyflie
@@ -131,23 +157,87 @@ class PidExample:
         except AttributeError:
             print "Could not add Stabilizer log config, bad configuration."
 
+        for group, name in self.param_var:
+            self._cf.param.add_update_callback(group = group, name = name,
+                    cb = self._param_callback)
+            self._cf.param.request_param_update("{0}.{1}".format(group, name))
+
+        ## Print the param TOC
+        #p_toc = self._cf.param.toc.toc
+        #for group in sorted(p_toc.keys()):
+        #    print "{}".format(group)
+        #    for param in sorted(p_toc[group].keys()):
+        #        print "\t{}".format(param)
+        #        self._param_check_list.append("{0}.{1}".format(group, param))
+        #    self._param_groups.append("{}".format(group))
+        #    # For every group, register the callback
+        #    self._cf.param.add_update_callback(group=group, name=None,
+        #                                       cb=self._param_callback)
+
+        #print
+        #print "Reading back back all parameter values"
+        # Request update for all the parameters using the full name
+        # group.name
+        #for p in self._param_check_list:
+        #    self._cf.param.request_param_update(p)
+        
+
         # Start a timer to disconnect in 10s
         #t = Timer(5, self._cf.close_link)
         #t.start()
         Thread(target=self._ramp_motors).start()
 
+    def _param_callback(self, name, value):
+        """Generic callback registered for all the groups"""
+        print "{0}: {1}".format(name, value)
+
+        # Remove each parameter from the list and close the link when
+        # all are fetched
+
+        #self._param_check_list.remove(name)
+        #if len(self._param_check_list) == 0:
+        #    print "Have fetched all parameter values."
+
+        #    # First remove all the group callbacks
+        #    for g in self._param_groups:
+        #        self._cf.param.remove_update_callback(group=g,
+        #                                              cb=self._param_callback)
+
+        #    # Create a new random value [0.00,1.00] for pid_attitude.pitch_kd
+        #    # and set it
+        #    pkd = random.random()
+        #    print
+        #    print "Write: pid_attitude.pitch_kd={:.2f}".format(pkd)
+        #    self._cf.param.add_update_callback(group="pid_attitude",
+        #                                       name="pitch_kd",
+        #                                       cb=self._a_pitch_kd_callback)
+        #    # When setting a value the parameter is automatically read back
+        #    # and the registered callbacks will get the updated value
+        #    self._cf.param.set_value("pid_attitude.pitch_kd",
+        #                             "{:.2f}".format(pkd))
+
+    #def _a_pitch_kd_callback(self, name, value):
+    #    """Callback for pid_attitude.pitch_kd"""
+    #    print "Readback: {0}={1}".format(name, value)
+    #    print
+
     def _ramp_motors(self):
         time.sleep(1)
+        self._cf.param.set_value("pid_attitude.pitch_kd", "0.1")
 
-        self.pitch = 0 #sum(self.pitch_data)/len(self.pitch_data)
-        self.roll = 0 #sum(self.roll_data)/len(self.roll_data)
-        self.yawrate = 0
+        #self.pitch = 0 #sum(self.pitch_data)/len(self.pitch_data)
+        #self.roll = 0 #sum(self.roll_data)/len(self.roll_data)
+        #self.yawrate = 0
 
         #Unlock startup thrust protection
         self._cf.commander.send_setpoint(0, 0, 0, 0)
 
         while True:
-            self._cf.commander.send_setpoint(self.roll, self.pitch, self.yawrate, self.thrust)
+            self._cf.commander.send_setpoint(
+                    self.control_data['roll'],
+                    self.control_data['pitch'], 
+                    self.control_data['yawrate'],
+                    self.control_data['thrust'])
             time.sleep(0.1)
 
         self._cf.commander.send_setpoint(0, 0, 0, 0)
@@ -214,15 +304,22 @@ class PidExample:
         self.t.start(50)
 
     def valueChanging(self, sb, value):
-        self.thrust = value
+        for i in range(len(self.control_spins)):
+            if self.control_spins[i] == sb:
+                print self.control_var[i]
+                self.control_data[self.control_var[i]] = value
+
+        for i in range(len(self.param_spins)):
+            if self.param_spins[i] == sb:
+                print self.param_var[i]
+                self._cf.param.set_value("{0}.{1}".format(self.param_var[i][0], self.param_var[i][1]), "{:.2f}".format(value))
+
+        #self.thrust = value
 
     def stopClicked(self):
-        self.thrust = 0
+        self.control_data['thrust'] = 0
 
     def _gui2(self):
-        spins = [
-            ("Thrust", pg.SpinBox(value=0, int=True, dec=False, minStep=500, step=500, bounds=[0, None]))
-        ]
 
         self.win2 = QtGui.QMainWindow()
         self.win2.setWindowTitle('Settings')
@@ -232,19 +329,30 @@ class PidExample:
         self.win2.setCentralWidget(cw)
         self.win2.show()
 
-        self.labels = []
-
-        for text, spin in spins:
-            label = QtGui.QLabel(text)
-            self.labels.append(label)
+# control inputs.
+        for name in self.control_var:
+            label = QtGui.QLabel(name)
+            spin  = pg.SpinBox(value=0, int=True, dec=False, minStep=500, step=500, bounds=[0, None])
             layout.addWidget(label)
             layout.addWidget(spin)
             spin.sigValueChanging.connect(self.valueChanging)
 
+            self.control_spins.append(spin)
+
         btn = QtGui.QPushButton("Stop")
         btn.clicked.connect(self.stopClicked)
         layout.addWidget(btn)
-        
+
+# params inputs.
+        for group, name in self.param_var:
+            label = QtGui.QLabel("{0}.{1}".format(group, name))
+            spin  = pg.SpinBox(value = 0, bounds=[0, None], step = 0.1, minStep = 0.1)
+            layout.addWidget(label)
+            layout.addWidget(spin)
+            spin.sigValueChanging.connect(self.valueChanging)
+
+            self.param_spins.append(spin)
+
 
     def _updateData(self):
         for i in range(len(self.log_data)):
