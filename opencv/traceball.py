2562074a5458db5bbf6f7bdf8771b2e149b25940
s33333123 <s33333123@gmail.com>
2015-07-02 20:44:54 +0800

    add area condition

--- opencv/traceball.py
+++ opencv/traceball.py
@@ -19,13 +19,14 @@ if __name__ == '__main__':
     prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
     show_img = False
     hsv_set = False
-    
+    max_area=-1
     ORANGE_MIN = np.array([0, 0, 0],np.uint8)
     ORANGE_MAX = np.array([255, 255, 255],np.uint8)
     cx=-1
     cy=-1
     px,py=-1,-1
     pre_cx, pre_cy=-1,-1
+    pre_area=-1
     timg = np.zeros((480,640,3), np.uint8)
     cv2.namedWindow('img')
     cv2.setMouseCallback('img',get_mouse_value)
@@ -54,31 +55,38 @@ if __name__ == '__main__':
                 mean = cv2.mean(hsv,mask = mask)
             '''
             #gradient = cv2.inRange(gradient, np.array([2,2,2]), np.array([255,255,255]))
-            erosion = cv2.erode(thresheld,kernel,iterations = 1)
+            erosion = cv2.dilate(thresheld,kernel,iterations = 1)
             contours,hierarchy = cv2.findContours(erosion, 1, 2)
-            erosion = cv2.cvtColor(thresheld,cv2.COLOR_GRAY2BGR)
+            thresheld = cv2.cvtColor(thresheld,cv2.COLOR_GRAY2BGR)
             max_area=0
+            max_cnt=0
             if hsv_set is False and len(contours) >0 :
                 for i in contours:
                     cnt = i-1
                     M = cv2.moments(cnt)
-                    if M['m00']>0 :#cv2.contourArea(cnt)>max_area:
-                        if cv2.contourArea(cnt)>max_area:
-                            max_area = cv2.contourArea(cnt)
-                        cx = int(M['m10']/M['m00'])
-                        cy = int(M['m01']/M['m00'])
-                        if (pre_cx<0 and pre_cy<0)or(math.fabs(cx-pre_cx)<20 and math.fabs(cx-pre_cy),20):
-                            pre_cx=cx
-                            pre_cy=cy
-                            cv2.circle(thresheld,(cx,cy),2,(0,0,255),3)
-                            epsilon = 0.1*cv2.arcLength(cnt,True)
-                            print cx,cy,epsilon,cv2.contourArea(cnt),'\n'                        
+                    if cv2.contourArea(cnt)>max_area:
+                        max_area=cv2.contourArea(cnt)
+                        max_cnt=cnt
+                M = cv2.moments(max_cnt)
+                if M['m00']>0 :
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+                    if (pre_cx<0 and pre_cy<0)or(math.fabs(cx-pre_cx)<20 and 
+                        math.fabs(cx-pre_cy),20 and 
+                        max_area>1000 or 
+                        math.fabs(max_area-pre_area)<10000):
+                        pre_cx=cx
+                        pre_cy=cy
+                        pre_area=max_area
+                        cv2.circle(thresheld,(cx,cy),2,(0,0,255),3)
+                        epsilon = 0.1*cv2.arcLength(max_cnt,True)
+                        print cx,cy,epsilon,max_area,'\n'                        
 
             else:
                 pre_cx, pre_cy = -1, -1
-                
+            cv2.imshow('thresheld', thresheld)    
             cv2.imshow('img', hsv)
-            cv2.imshow('thresheld', thresheld)
+
             prevgray = gray
             
 
@@ -91,6 +99,7 @@ if __name__ == '__main__':
                 hsv_set=False
             if ch == ord('c'):
                 print "reset"
+                pre_x,pre_y=-1,-1
                 if(ix>0 and iy>0):
                    ORANGE_MIN=hsv[iy][ix]-np.array([2,2,2])
                    ORANGE_MAX=hsv[iy][ix]+np.array([2,2,2])
