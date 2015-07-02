d12641e9883e7226a1ee223d7822a37cf88a2e25
s33333123 <s33333123@gmail.com>
2015-07-02 16:49:19 +0800

    update

--- opencv/traceball.py
+++ opencv/traceball.py
@@ -3,6 +3,13 @@ import numpy as np
 import cv2
 import sys
 import math
+from matplotlib import pyplot as plt
+ix,iy = -1, -1
+
+def get_mouse_value(event,x,y,flags,param):
+    global ix,iy
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        ix, iy = x,y
 
 
 if __name__ == '__main__':
@@ -11,43 +18,82 @@ if __name__ == '__main__':
     ret, prev = cam.read()
     prevgray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
     show_img = False
-    ORANGE_MIN = np.array([100, 140, 100],np.uint8)
-    ORANGE_MAX = np.array([120, 230, 255],np.uint8)
-    cx=0
-    cy=0
+    hsv_set = False
+    
+    ORANGE_MIN = np.array([0, 0, 0],np.uint8)
+    ORANGE_MAX = np.array([255, 255, 255],np.uint8)
+    cx=-1
+    cy=-1
+    px,py=-1,-1
+    pre_cx, pre_cy=-1,-1
+    timg = np.zeros((480,640,3), np.uint8)
+    cv2.namedWindow('img')
+    cv2.setMouseCallback('img',get_mouse_value)
+
     while True:
         ret, img = cam.read()#读取视频的下一帧作为光流输入的当前帧
         if ret == True:#判断视频是否结束
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-            hsv = cv2.medianBlur(hsv,81)
+            hsv = cv2.medianBlur(hsv,51)
             if cv2.cv.WaitKey(10)==27:
                 break
+            
             thresheld = cv2.inRange(hsv, ORANGE_MIN, ORANGE_MAX)
-            #thresheld = cv2.medianBlur(thresheld,5)
             cimg = cv2.cvtColor(thresheld,cv2.COLOR_GRAY2BGR)
             kernel = np.ones((2,2),np.uint8)
-            erosion = cv2.erode(thresheld,kernel,iterations = 5)
+            '''
+            edges = cv2.Canny(hsv,100,50)
+            cv2.imshow("edges", edges)
+            contours,hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            if len(contours)>0:
+                cv2.drawContours(hsv, contours,- 1 ,( 0 , 255 , 0 ), 3)
+            for h,cnt in enumerate(contours):
+                mask = np.zeros(gray.shape,np.uint8)
+                cv2.drawContours(hsv,[cnt],0,(255,0,0),-1)
+                mean = cv2.mean(hsv,mask = mask)
+            '''
+            #gradient = cv2.inRange(gradient, np.array([2,2,2]), np.array([255,255,255]))
+            erosion = cv2.erode(thresheld,kernel,iterations = 1)
             contours,hierarchy = cv2.findContours(erosion, 1, 2)
+            erosion = cv2.cvtColor(thresheld,cv2.COLOR_GRAY2BGR)
             max_area=0
-            if len(contours) >0 :
+            if hsv_set is False and len(contours) >0 :
                 for i in contours:
                     cnt = i-1
                     M = cv2.moments(cnt)
-                    if M['m00']>0 and cv2.contourArea(cnt)>max_area:
-                        max_area = cv2.contourArea(cnt)
+                    if M['m00']>0 :#cv2.contourArea(cnt)>max_area:
+                        if cv2.contourArea(cnt)>max_area:
+                            max_area = cv2.contourArea(cnt)
                         cx = int(M['m10']/M['m00'])
                         cy = int(M['m01']/M['m00'])
-                        erosion = cv2.cvtColor(thresheld,cv2.COLOR_GRAY2BGR)
-                        cv2.circle(erosion,(cx,cy),2,(0,0,255),3)
-                        epsilon = 0.1*cv2.arcLength(cnt,True)
-                        print cx,cy,epsilon,cv2.contourArea(cnt),'\n'
-            cv2.imshow('flow', erosion)
-            if show_img:
-                cv2.circle(img,(cx,cy),2,(0,0,255),3)
-                cv2.imshow('img', img)
+                        if (pre_cx<0 and pre_cy<0)or(math.fabs(cx-pre_cx)<20 and math.fabs(cx-pre_cy),20):
+                            pre_cx=cx
+                            pre_cy=cy
+                            cv2.circle(thresheld,(cx,cy),2,(0,0,255),3)
+                            epsilon = 0.1*cv2.arcLength(cnt,True)
+                            print cx,cy,epsilon,cv2.contourArea(cnt),'\n'                        
+
+            else:
+                pre_cx, pre_cy = -1, -1
+                
+            cv2.imshow('img', hsv)
+            cv2.imshow('thresheld', thresheld)
+            prevgray = gray
             
+
             ch = 0xFF & cv2.waitKey(5)
+            if ch == ord('z'):
+                print "set"
+                hsv_set=True
+            if ch == ord('x'):
+                print "set off"
+                hsv_set=False
+            if ch == ord('c'):
+                print "reset"
+                if(ix>0 and iy>0):
+                   ORANGE_MIN=hsv[iy][ix]-np.array([2,2,2])
+                   ORANGE_MAX=hsv[iy][ix]+np.array([2,2,2])
             if ch == ord('q'):
                 break
             if ch == ord('1'):
@@ -58,7 +104,29 @@ if __name__ == '__main__':
                 cv2.imwrite('hsv.jpg', hsv)
                 cv2.imwrite('img.jpg', img)
                 print '--save--'
-            
+            if hsv_set:
+                if ix <0 or iy<0 or (ix==px and iy==py):
+                    print "click"
+                    continue
+                print ix,iy,hsv[iy][ix],ORANGE_MIN,ORANGE_MAX
+                if (ORANGE_MIN[0]>hsv[iy][ix][0]):
+                    ORANGE_MIN[0]=hsv[iy][ix][0]
+                elif(ORANGE_MAX[0]<hsv[iy][ix][0]):
+                    ORANGE_MAX[0]=hsv[iy][ix][0]
+                if (ORANGE_MIN[1]>hsv[iy][ix][1]):
+                    ORANGE_MIN[1]=hsv[iy][ix][1]
+                elif(ORANGE_MAX[1]<hsv[iy][ix][1]):
+                    ORANGE_MAX[1]=hsv[iy][ix][1]
+                if (ORANGE_MIN[2]>hsv[iy][ix][2]):
+                    ORANGE_MIN[2]=hsv[iy][ix][2]
+                elif(ORANGE_MAX[2]<hsv[iy][ix][2]):
+                    ORANGE_MAX[2]=hsv[iy][ix][2]
+                px=ix
+                py=iy
+                print px,py,hsv[iy][ix],ORANGE_MIN,ORANGE_MAX,'\n'
+                
+            if show_img:
+                cv2.imshow('img', img)
 
 
     cv2.destroyAllWindows()             
